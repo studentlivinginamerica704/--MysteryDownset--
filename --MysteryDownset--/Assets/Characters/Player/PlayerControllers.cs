72134a436eeece0a8aaafa71d4cfb91406f17026
studentlivinginamerica704 <142178968+studentlivinginamerica704@users.noreply.github.com>
2023-11-04 11:36:17 -0400

    Playercontrollers

--- --MysteryDownset--/Assets/Characters/Player/PlayerControllers.cs
+++ --MysteryDownset--/Assets/Characters/Player/PlayerControllers.cs
@@ -3,104 +3,98 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-// Takes and handles input and movement for a player character
 public class PlayerController : MonoBehaviour
 {
+    [Header("Movement Settings")]
     public float moveSpeed = 1f;
     public float collisionOffset = 0.05f;
     public ContactFilter2D movementFilter;
+
+    [Header("References")]
     public SwordAttack swordAttack;
 
-    Vector2 movementInput;
-    SpriteRenderer spriteRenderer;
-    Rigidbody2D rb;
-    Animator animator;
-    List<RaycastHit2D> castCollisions = new List<RaycastHit2D>();
+    private Vector2 movementInput;
+    private Rigidbody2D rb;
+    private Animator animator;
+    private SpriteRenderer spriteRenderer;
+    private bool canMove = true;
 
-    bool canMove = true;
-
-    // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
-        spriteRenderer = GetComponent<SpriteRenderer>();
+        spriteRenderer = GetComponent < SpriteRenderer();
     }
 
     private void FixedUpdate()
     {
         if (canMove)
         {
-            // If movement input is not 0, try to move
-            if (movementInput != Vector2.zero)
-            {
-
-                bool success = TryMove(movementInput);
-
-                if (!success)
-                {
-                    success = TryMove(new Vector2(movementInput.x, 0));
-                }
-
-                if (!success)
-                {
-                    success = TryMove(new Vector2(0, movementInput.y));
-                }
-
-                animator.SetBool("isMoving", success);
-            }
-            else
-            {
-                animator.SetBool("isMoving", false);
-            }
-
-            // Set direction of sprite to movement direction
-            if (movementInput.x < 0)
-            {
-                spriteRenderer.flipX = true;
-            }
-            else if (movementInput.x > 0)
-            {
-                spriteRenderer.flipX = false;
-            }
+            UpdateMovement();
         }
+    }
+
+    private void UpdateMovement()
+    {
+        movementInput = GetMovementInput();
+
+        bool success = TryMove(movementInput);
+
+        if (!success)
+        {
+            success = TryMove(new Vector2(movementInput.x, 0));
+        }
+
+        if (!success)
+        {
+            success = TryMove(new Vector2(0, movementInput.y));
+        }
+
+        UpdateAnimator(success);
+        UpdateSpriteDirection();
+    }
+
+    private Vector2 GetMovementInput()
+    {
+        return Keyboard.current.wKey.isPressed ? Vector2.up : Vector2.zero;
+        // Add more input handling logic as needed (e.g., using the Input System).
     }
 
     private bool TryMove(Vector2 direction)
     {
-        if (direction != Vector2.zero)
+        if (direction == Vector2.zero)
         {
-            // Check for potential collisions
-            int count = rb.Cast(
-                direction, // X and Y values between -1 and 1 that represent the direction from the body to look for collisions
-                movementFilter, // The settings that determine where a collision can occur on such as layers to collide with
-                castCollisions, // List of collisions to store the found collisions into after the Cast is finished
-                moveSpeed * Time.fixedDeltaTime + collisionOffset); // The amount to cast equal to the movement plus an offset
-
-            if (count == 0)
-            {
-                rb.MovePosition(rb.position + direction * moveSpeed * Time.fixedDeltaTime);
-                return true;
-            }
-            else
-            {
-                return false;
-            }
-        }
-        else
-        {
-            // Can't move if there's no direction to move in
-            return false;
+            return false; // Can't move if there's no direction to move in.
         }
 
+        RaycastHit2D[] hits = new RaycastHit2D[1];
+        int hitCount = rb.Cast(direction, movementFilter, hits, moveSpeed * Time.fixedDeltaTime + collisionOffset);
+
+        if (hitCount == 0)
+        {
+            rb.MovePosition(rb.position + direction * moveSpeed * Time.fixedDeltaTime);
+            return true;
+        }
+
+        return false;
     }
 
-    void OnMove(InputValue movementValue)
+    private void UpdateAnimator(bool isMoving)
+    {
+        animator.SetBool("isMoving", isMoving);
+    }
+
+    private void UpdateSpriteDirection()
+    {
+        spriteRenderer.flipX = movementInput.x < 0;
+    }
+
+    public void OnMove(InputValue movementValue)
     {
         movementInput = movementValue.Get<Vector2>();
     }
 
-    void OnFire()
+    public void OnFire()
     {
         animator.SetTrigger("swordAttack");
     }
@@ -109,7 +103,7 @@ public class PlayerController : MonoBehaviour
     {
         LockMovement();
 
-        if (spriteRenderer.flipX == true)
+        if (spriteRenderer.flipX)
         {
             swordAttack.AttackLeft();
         }
